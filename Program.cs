87be7e9827e4cff9fc5f1c9c756420dae9be5350
uday pratap <uday33888@gmail.com>
2026-02-11 13:36:38 +0530

    day1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //write your answer here
+
+        Console.Write("Hello, World!");
+
+    }
+
+
+}
---
+++ p2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Prompt user for first number
+        Console.WriteLine("Enter the first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        // Prompt user for second number
+        Console.WriteLine("Enter the second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Display result
+        Console.WriteLine("The sum is: " + sum);
+    }
+}
